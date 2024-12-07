28c7f37bbd5ee256e45abea82f8ac52344882b4f
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2024-12-07 21:01:26 +0100

    Install tmux with plugins

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 #
 # Variables
 #
+dotfiles_repo="git@github.com:dylanvgils/dotfiles.git"
 dotfiles_dir="$HOME/dotfiles"
 
+config_dir="$HOME/.config"
+
+omzsh_install_script="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
 
 #
 # Functions
@@ -47,7 +51,7 @@ if [ -z "${ZSH+x}" ]; then
   export CHSH="no"
   export RUNZSH="no"
 
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL $omzsh_install_script)"
 else
   log_info ".oh-my-zsh directory exists, skipping install"
 fi
@@ -57,15 +61,15 @@ if [ -d "$dotfiles_dir" ]; then
   log_info "Make sure latest changes are pulled"
   git pull origin main
 else
-  log_info "Cloning dotfiles repostiory in $dotfiles_dir"
-  git clone git@github.com:dylanvgils/dotfiles.git $dotfiles_dir
+  log_info "Cloning dotfiles repository ($dotfiles_repo) in $dotfiles_dir"
+  git clone $dotfiles_repo $dotfiles_dir
 fi
 
 # Remove existing configuration files so that they can be overriden later
 log_header "Clean existing configuration"
 log_info "Remove default config files"
 
-config_files_to_remove=".aliases .completions .functions .zshenv .zshrc .zshrc.pre-oh-my-zsh"
+config_files_to_remove=".aliases .completions .functions .zshenv .zshrc .zshrc.pre-oh-my-zsh .tmux.conf"
 for item in $config_files_to_remove; do
     file="$HOME/$(echo "$item" | xargs)"
     if [ -f "$file" ]; then
@@ -80,6 +84,13 @@ log_header "Apply dotfile configurations"
 cd $dotfiles_dir
 log_info "Soruce configuration files using included Makefile"
 make init
+
+# Install tmux
+log_header "Install tmux"
+sudo apt install -y tmux
+
+log_info "Install tmux plugins"
+$config_dir/tmux/plugins/tpm/bin/install_plugins
 
 # Run the new shell
 $SHELL
