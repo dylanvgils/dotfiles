271fd5be74af6b4641efa4a6ee149bdcc2f4466a
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2024-12-11 21:40:29 +0100

    Update plugin config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
 vim.cmd([[colorscheme onedark]])
--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -4,6 +4,9 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      { "<leader>e", ":NvimTreeToggle<cr>", desc = "Nvim Tree Toggle" },
+    },
     config = function()
       require("nvim-tree").setup()
     end,
