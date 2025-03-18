6279c5eaaa3b0c156d5c652d0e0fa4d10051b640
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2025-03-18 19:56:38 +0100

    Update mason config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,4 @@ vim.g.loaded_netrwPlugin = 1
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd([[colorscheme onedark]])
 vim.opt.clipboard = "unnamedplus"
--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -6,7 +6,6 @@ return {
       "neovim/nvim-lspconfig",
       "WhoIsSethDaniel/mason-tool-installer.nvim",
     },
-    opts = { ensure_installed = { "prettier" } },
     config = function()
       require("mason").setup({
         ui = {
