5bbd74b96ece721a55c64b4c342f60c1f67d9a60
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2025-03-19 16:12:21 +0100

    Simply lazy vim config

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,49 +1,14 @@
 return {
   {
     "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     },
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "tsserver",
-          "html",
-          "cssls",
-          "ts_ls",
-          "tailwindcss",
-          -- "gopls",
-          "templ",
-        },
-        automatic_installation = true,
-      })
-
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "lua-language-server",
-          "vim-language-server",
-          "typescript-language-server",
-          "prettier",
-          "stylua",
-          -- "gopls",
-          -- "templ",
-          "eslint_d",
-        },
-      })
-
-      require("lspconfig").tailwindcss.setup({})
-    end,
   },
 }
