83b45237b0d2ef50d05bde05be9073a23b2e84d5
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2025-11-01 11:34:54 +0100

    Update neotest deps

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
     "marilari88/neotest-vitest",
+    "nvim-treesitter/nvim-treesitter",
   },
   opts = {
     adapters = {
