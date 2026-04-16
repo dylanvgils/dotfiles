7631e1f22570c85c60d5066cf08d7c2da45e1bab
Dylan van Gils <9094916+dylanvgils@users.noreply.github.com>
2026-04-16 06:38:38 +0200

    Update vim config

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -7,8 +7,18 @@ return {
     keys = {
       { "<leader>e", ":NvimTreeToggle<cr>", desc = "Nvim Tree Toggle" },
     },
+    opts = {},
     config = function()
-      require("nvim-tree").setup({ view = { adaptive_size = true } })
+      require("nvim-tree").setup({
+        view = { adaptive_size = true },
+        update_focused_file = {
+          enable = true,
+          update_root = {
+            enable = false,
+            ignore_list = {},
+          },
+        },
+      })
     end,
   },
 }
